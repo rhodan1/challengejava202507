3586b38ff6b8b573cb61339b03d404af7433ae8d
Rodrigo Porras <rhodan1@gmail.com>
2025-07-03 02:27:08 -0400

    Remove comments

--- usuariosAPI/src/main/java/cl/rporras/desafio2025/usuariosapi/config/WebConfig.java
+++ usuariosAPI/src/main/java/cl/rporras/desafio2025/usuariosapi/config/WebConfig.java
@@ -5,18 +5,5 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class WebConfig {
 
-    //TODO: Habilitar filtro de Validacion JWT
-
-//    @Autowired
-//    private JwtAuthenticationFilter jwtAuthenticationFilter;
-//
-//    @Bean
-//    public FilterRegistrationBean<JwtAuthenticationFilter> jwtFilter(JwtProvider jwtProvider) {
-//        FilterRegistrationBean<JwtAuthenticationFilter> registration = new FilterRegistrationBean<>();
-//        registration.setFilter(new JwtAuthenticationFilter(jwtProvider));
-//        registration.addUrlPatterns("/*"); // rutas protegidas
-//        registration.setOrder(1);
-//        return registration;
-//    }
 }
 
